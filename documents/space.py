690f32e39bd3a16f73d5353218e3334bc6178d38
toto-anc <theo.anchev@gmail.com>
2024-09-27 15:57:34 +0200

    Add files via upload

---
+++ documents/space.py
@@ -0,0 +1,26 @@
+import pygame  # necessaire pour charger les images et les sons
+
+
+class Joueur() : # classe pour créer le vaisseau du joueur
+    def __init__(self) :
+        self.image=pygame.image.load('vaisseau.png')
+        self.vitesse=0.5
+        self.position=400
+        self.sens="centre" 
+    def deplacer(self):
+        if self.sens=="droite"and self.position<800-64:
+            
+            self.position+=self.vitesse
+            
+            
+        elif self.sens=="gauche"and self.position>0:
+            self.position-=self.vitesse
+            
+class Balle():
+    def __init__(self):
+        self.tireur=Joueur
+        self.image=pygame.image.load("balle.png")
+        self.depart=self.position
+        self.hauteur=
+        
+        
